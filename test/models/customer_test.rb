0def185777cb2c8cc1bfa73c4912a988739c6256
Jasmine Quach <xjasminequach@gmail.com>
2017-11-02 18:32:10 -0500

    WIP Customers model testing

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,15 +1,24 @@
 require 'minitest/autorun'
-require_relative '../app/models/customer.rb'
+require_relative '../../app/models/customer.rb'
 
 
-class Customer
+class CustomerTest < Minitest::Test
 
 
   # setup
 
   #test initialize that creates instance of customer
+  def test_initialize
+    @new_customer = Customer.new('name_first', 'name_last', 'address_street', 'address_email', 'city', 'state', 'zip_code', 'active')
+    assert_instance_of Customer, @new_customer #checking type
+  end
 
-  # testmcreate customer sql
+  # test create function customer sql
+  def test_create_customer
+    @new_customer = Customer.new('name_first', 'name_last', 'address_street', 'address_email', 'city', 'state', 'zip_code', 'active')
+    customerid = @new_customer.create
+    assert_operator customerid, :>, 0 #return the id of what's created as long as greater than zero
+  end
 
   # test query all customers
 
